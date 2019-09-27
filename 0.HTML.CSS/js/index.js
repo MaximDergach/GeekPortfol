c2575c3a96fa7d07ca76fcb415778013f2d72fb2
Maxim Dergach <makder2008@mail.ru>
2019-09-27 18:43:30 +0700

    add file js оченьинтересно.js

---
+++ 0.HTML.CSS/js/index.js
@@ -0,0 +1,18 @@
+var blueRect = document.getElementsByClassName("btn_arrow");
+
+var num = 0
+
+function cliker() {
+	console.log(blueRect)
+
+	num++
+
+	console.log(num)
+}
+addEventListener('click', cliker 
+
+	)
+
+
+
+
